74c0c6333fc377809f5222b78e51ae0ad830bf1a
Mark Thorne <thorne.mark@gmail.com>
2023-01-08 23:58:43 -0600

    Practice with lists

--- playground.py
+++ playground.py
@@ -1,2 +1,9 @@
-books, authors, genres = 14, 11, 5
-print(books)
+authors = ["Shakespeare", "Proust", "Tolstoy", "Barton"]
+print(authors)
+authors.append("Smartypants")
+
+for a in authors:
+  print(f"You should read {a}.")
+
+del authors[2]
+print(authors)
