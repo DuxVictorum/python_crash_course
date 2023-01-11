1ff9fc292c2351adfc845d13bae72910dd891465
Mark Thorne <thorne.mark@gmail.com>
2023-01-11 00:03:29 -0600

    Finished 3_8

--- ch03/3_8-seeing_the_world.py
+++ ch03/3_8-seeing_the_world.py
@@ -11,5 +11,13 @@ print("The order of items has still not changed!", places_to_go)
 # Reversing the list permanently
 places_to_go.reverse()
 print("\n...But now it has.", places_to_go)
+places_to_go.reverse()
+print("And now it's back again with another reverse.", places_to_go)
+
+# Sorting the list permanently (and alphabetically)
+places_to_go.sort()
+print("Now the list is sorted alphabetically, for good.", places_to_go)
+places_to_go.sort(reverse=True)
+print("And once again it's been reversed, for good.", places_to_go)
 
 
