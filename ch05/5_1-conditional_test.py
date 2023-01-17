15c6700f8a98dcfca457374bd924a8724d95927e
Mark Thorne <thorne.mark@gmail.com>
2023-01-16 23:09:18 -0600

    First practice with conditionals

---
+++ ch05/5_1-conditional_test.py
@@ -0,0 +1,27 @@
+# Series of 10 Boolean conditions for practice
+
+# 1
+book1 = "Python Crash Course"
+print(f"I hear you want {book1}. Is that true?")
+print(book1 == "python crash course".title())
+
+#2
+book2 = "AWS Certified Solutions Architect Study Guide"
+print("Is it true that you're enjoying that exam study guide? Or was it another book?")
+print(book2 == "AWS Certified Solutions Architect Study Guide")
+
+#3
+book3 = "Deep Work"
+print("Are you reading Deep Work right now?")
+print(book3 == "Deep Work")
+
+#4
+book4 = "Istio In Action"
+signed_book = True
+print(f"Did you actually get the author to sign your {book4} book?")
+print(signed_book == True)
+
+#5
+book5 = "The Lord of The Rings"
+print(f"Is your favorite book {book5}?")
+print(book5 == "The Lord of The Rings")
