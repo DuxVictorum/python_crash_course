552544e0f992318af77caddb8d325f779a50e21a
Mark Thorne <thorne.mark@gmail.com>
2023-01-24 00:11:24 -0600

    Finished 6_1

--- ch06/6_1-person.py
+++ ch06/6_1-person.py
@@ -2,4 +2,10 @@
 cool_guy = {
     'first_name': 'Charles',
     'last_name': 'Thorne',
-    'age': 39}
+    'age': 14,
+    'city': 'Pawston'}
+
+# Print out dictionary information
+print(f"Let's meet our first character, {cool_guy['first_name']} \
+{cool_guy['last_name']} from {cool_guy['city']}. He is {cool_guy['age']} \
+years old.")
