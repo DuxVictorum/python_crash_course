e068ffdfa371a1dd2dfe5b549af3e2970b36b858
Mark Thorne <thorne.mark@gmail.com>
2023-01-25 00:10:15 -0600

    6_2 on dictionaries

---
+++ ch06/6_2-favorite_numbers.py
@@ -0,0 +1,15 @@
+# Initialize a dictionary of favorite numbers
+fav_numbers = {
+    'Sarah': 24,
+    'Steve': 12,
+    'Shania': 8,
+    'Sarbanian': 2,
+    'Sally': 3.14159
+}
+
+# Print out their favorite numbers!
+print(f"Sarah loves the number {fav_numbers['Sarah']}!")
+print(f"Steve loves the number {fav_numbers['Steve']}!")
+print(f"Shania loves the number {fav_numbers['Shania']}!")
+print(f"Sarbanian loves the number {fav_numbers['Sarbanian']}!")
+print(f"And Sally loves the number {fav_numbers['Sally']}!")
