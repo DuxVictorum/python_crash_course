0e0a336248e94ca93d3f192e7bd6141e52edc939
Mark Thorne <thorne.mark@gmail.com>
2023-01-27 23:58:25 -0600

    Started 6_8

---
+++ ch06/6_8-pets.py
@@ -0,0 +1,22 @@
+# Initialize three dictionaries about pets
+pet1 = {
+    "name": "Fluffy",
+    'type': 'hamster',
+    'owner': 'Ricky'
+}
+pet2 = {
+    "name": "Harkbark",
+    'type': 'dog',
+    'owner': 'Alison'
+}
+pet3 = {
+    "name": "Raazor",
+    'type': 'dragon',
+    'owner': 'Lars'
+}
+pets = [pet1, pet2, pet3]
+
+# Print out info on each pet
+print("Here are our amazing neighborhood pets:")
+for pet in pets:
+    print(f"")
