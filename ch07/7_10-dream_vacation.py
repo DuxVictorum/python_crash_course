3dbe87b450eca0d552130da7a1aa82e97dd68cd3
Thorne (US), Mark <thorne.mark@gmail.com>
2023-02-04 00:18:11 -0600

    Finished 7_10

--- ch07/7_10-dream_vacation.py
+++ ch07/7_10-dream_vacation.py
@@ -1,9 +1,20 @@
-# Use a while loop to ask people about their dream vacation
+# Initialize variables
 still_asking = True
 results = []
 
+# Use a while loop to ask people about their dream vacation
 while still_asking:
-    name = input("What's your name? \n")
-    dream_destination = input("Where would your dream vacation trip take you? \n")
-    
+    responder = input("What's your name? ")
+    dream_destination = input("Where would your dream vacation trip take you? ")
+    response = {'name': responder, 'dest': dream_destination}
+    results.append(response)
 
+    # Ask whether another person would like to answer
+    repeat = input("Should I ask someone else? (yes/no) ")
+    if repeat.lower() == 'no':
+        still_asking = False
+
+# Print results
+print("\nOkay, tabulating results...\n")
+for result in results:
+    print(f"{result['name'].title()} would like to go to {result['dest']}.")
