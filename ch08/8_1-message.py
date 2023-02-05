335d3c10077a7a4242d9763f1ace102e9eccce96
Mark Thorne <thorne.mark@gmail.com>
2023-02-04 23:33:45 -0600

    Ch 8_1-2; basic functions

---
+++ ch08/8_1-message.py
@@ -0,0 +1,7 @@
+# Define a basic function to print a message
+def display_message(topic):
+    """Print a basic message"""
+    print(f"In this chapter, we will learn about {topic}.")
+
+# Call the function
+display_message('Latin participles')
---
+++ ch08/8_2-favorite_book.py
@@ -0,0 +1,7 @@
+# Define a function to describe a favorite book
+def favorite_book(title, author):
+    """Describe a favorite book"""
+    print(f"I really loved reading {title} by {author}.")
+
+# Call the function
+favorite_book('The Lord of the Rings', 'J.R.R. Tolkien')
